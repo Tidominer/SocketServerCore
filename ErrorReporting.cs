27edf5e6ce295e2615f884a725167dd8fca70823
Tidominer <75866836+Tidominer@users.noreply.github.com>
2026-05-15 03:10:11 +0330

    change: removed Details from ErrorMessage dto

--- ErrorReporting.cs
+++ ErrorReporting.cs
@@ -28,5 +28,4 @@ public class ErrorResponse
 {
     public string ErrorType { get; set; } = string.Empty;
     public string Message { get; set; } = string.Empty;
-    public string? Details { get; set; }
 }
